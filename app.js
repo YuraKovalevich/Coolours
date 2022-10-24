89736d47e9c0dffbb954f12407f0381cb4eac8fb
YuraKovalevich <kovalevichyura@gmail.com>
2022-10-24 20:50:04 +0300

    Feat: added script switch color by space

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const cols = document.querySelectorAll ('.col');
+document.addEventListener('keydown',event=>{
+    if(event.code.toLowerCase()==='space'){
+        setRandomColors();
+    }
+})
 
 function generateRandomColor() {
     const hexCodes = '0123456789ABCDEF';
@@ -12,10 +17,19 @@ function generateRandomColor() {
 function setRandomColors() {
     cols.forEach ((col) => {
         const text = col.querySelector ('h2');
-        const color = generateRandomColor ();
+        const button = col.querySelector ('button');
+
+        const color = chroma.random ();
         text.textContent = color;
         col.style.background = color;
+        setTextColor(text,color);
+        setTextColor(button,color);
     })
 }
 
-setRandomColors ();
+function setTextColor(text, color) {
+    const luminance = chroma (color).luminance ();
+    text.style.color = luminance > 0.5 ? 'black' : 'white';
+}
+setRandomColors();
+
