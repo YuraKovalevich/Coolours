2fe4fdc44cee96e838386d335013fc4bb0556f77
YuraKovalevich <kovalevichyura@gmail.com>
2022-10-24 21:11:52 +0300

    Feat:added fa-lock

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const cols = document.querySelectorAll ('.col');
-document.addEventListener('keydown',event=>{
-    if(event.code.toLowerCase()==='space'){
-        setRandomColors();
+document.addEventListener ('keydown', event => {
+    event.preventDefault();
+    if (event.code.toLowerCase () === 'space') {
+        setRandomColors ();
+    }
+})
+document.addEventListener ('click', (event) => {
+    const type = event.target.dataset.type;
+    if (type === 'lock') {
+        const node = event.target.tagName.toLowerCase () === 'i' ? event.target : event.target.children[0];
+        node.classList.toggle('fa-lock-open')
+        node.classList.toggle('fa-lock')
     }
 })
 
@@ -16,14 +25,15 @@ function generateRandomColor() {
 
 function setRandomColors() {
     cols.forEach ((col) => {
+        const isLocked=col.querySelector('i').classList.contains('fa-lock');
         const text = col.querySelector ('h2');
         const button = col.querySelector ('button');
-
         const color = chroma.random ();
+        if(isLocked) return;
         text.textContent = color;
         col.style.background = color;
-        setTextColor(text,color);
-        setTextColor(button,color);
+        setTextColor (text, color);
+        setTextColor (button, color);
     })
 }
 
@@ -31,5 +41,6 @@ function setTextColor(text, color) {
     const luminance = chroma (color).luminance ();
     text.style.color = luminance > 0.5 ? 'black' : 'white';
 }
-setRandomColors();
+
+setRandomColors ();
 
